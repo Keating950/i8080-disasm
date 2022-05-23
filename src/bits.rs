633d95511c4cb3f0a3be668b3b0e2b41998f3f03
Keating Reid <keating.reid@protonmail.com>
2022-05-23 15:44:04 -0400

    changing shifting for Bits::bit_range; adding tests

--- src/bits.rs
+++ src/bits.rs
@@ -13,6 +13,26 @@ impl Bits {
     }
 
     pub fn bit_range(&self, range: Range<u8>) -> u8 {
-        range.fold(0, |acc, offset| acc | (self.0 & (1 << offset)))
+        let start = range.start;
+        range.fold(0, |acc, offset| acc | (self.0 & (1 << offset))) >> start
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_bit_range() {
+        let cases = [(0b11100100, (0, 4), 0b0100), (0b11100100, (4, 8), 0b1110)];
+        for (val, (low, high), output) in cases {
+            let bits = Bits(val);
+            let bit_range = bits.bit_range(low..high);
+            assert_eq!(
+                bit_range, output,
+                "\nInput:\t{:08b}\nRange:\t{}..{}\nLeft:\t{:04b}\nRight:\t{:04b}",
+                val, low, high, bit_range, output
+            )
+        }
     }
 }
