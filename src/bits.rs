33b870f4ff332ad78d7b13d62af63e509a0c18d7
Keating Reid <keating.reid@protonmail.com>
2022-06-11 10:30:13 -0400

    revamping Bits to fix bugs

--- src/bits.rs
+++ src/bits.rs
@@ -1,20 +1,25 @@
 use std::ops::Range;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub struct Bits(pub u8);
+#[repr(transparent)]
+pub struct Bits(u8);
 
 impl Bits {
-    pub fn bit(&self, offset: usize) -> u8 {
-        debug_assert!(
-            offset < 8,
-            "argument 'offset' to Bits::bit cannot be greater than 7"
-        );
-        (self.0 & (1 << offset)) >> offset
+    pub const fn new(n: u8) -> Bits {
+        Bits(n)
+    }
+
+    pub const fn bit(&self, n: u8) -> u8 {
+        (self.0 & (1 << n)) >> n
     }
 
     pub fn bit_range(&self, range: Range<u8>) -> u8 {
-        let start = range.start;
-        range.fold(0, |acc, offset| acc | (self.0 & (1 << offset))) >> start
+        let end = 8 - range.end;
+        let mut val = 0;
+        for offset in range.rev().map(|n| 7 - n) {
+            val |= self.0 & (1 << offset);
+        }
+        val >> end
     }
 }
 
@@ -23,16 +28,8 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_bit_range() {
-        let cases = [(0b11100100, (0, 4), 0b0100), (0b11100100, (4, 8), 0b1110)];
-        for (val, (low, high), output) in cases {
-            let bits = Bits(val);
-            let bit_range = bits.bit_range(low..high);
-            assert_eq!(
-                bit_range, output,
-                "\nInput:\t{:08b}\nRange:\t{}..{}\nLeft:\t{:04b}\nRight:\t{:04b}",
-                val, low, high, bit_range, output
-            )
-        }
+    fn test_bits() {
+        let bits = Bits(0b10110000);
+        assert_eq!(bits.bit_range(0..2), 0b10);
     }
 }
